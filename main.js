73087ab84ac81bbc6309159fa02a122b03eb6c16
Acafac1994 <120322748+Acafac1994@users.noreply.github.com>
2022-12-11 21:47:44 +0100

    Kino.Kassa.programm

---
+++ main.js
@@ -0,0 +1,36 @@
+// If Stetements
+
+function main () {
+
+
+var input = document.getElementById("age");
+
+var age = input.value; 18 
+
+
+if (age <0 || age >120){
+    alert("Bitte Trage dein richtiges Alter ein")
+} else if (age <6 || age >60) {
+    alert("der Eintritt beträht 6,50€")
+} else {
+    alert("Der Preis beträgt 7.50€")
+}
+
+
+// Notizen
+
+// Was wird gemcht ?
+
+// Für alle ander -> 7,50 €
+// < 6 Jahre oder > 60 -> 6,50€
+
+// der ODER operator = ||
+// Um eine IF else abfrage zu machen immer zu erst IF dann klammer auf den den namen der viable angeben und dann die funtion )
+// If (age <6)
+// um einen Alert auszulösen eifanch alert und in klamer den text der ausgegeben soll eingeben mit"wenn String
+
+
+
+
+
+}
